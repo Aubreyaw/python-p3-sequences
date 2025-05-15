0e138ed37426d9d22f25802ba513a4133a8cc9c7
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-14 21:52:21 -0400

    Completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fib_list = [0, 1]
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    for i in range(2, length):
+        next_num = fib_list[-1] + fib_list[-2]
+        fib_list.append(next_num)
+    print(fib_list)
     pass
